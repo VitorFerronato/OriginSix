8d08936c4ffb195a2e29db13685cc5ae98d1101e
Vitor Ferronato <vitorferronato@gmail.com>
2022-04-06 09:47:25 -0300

    Settings

--- main.js
+++ main.js
@@ -19,19 +19,6 @@ for(const link of links) {
 }
 
 
-// Shadown header scroll
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function() {
-    if(this.window.scrollY >= navHeight) {
-        header.classList.add('scroll')
-    } else {
-        header.classList.remove('scroll')
-    }
-})
-
-
 // Testimonials Swiper
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
@@ -39,7 +26,13 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-pagination'
     },
     mouseWheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
   });
   
 
@@ -53,8 +46,64 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(
     `#home .image, #home .text,
-    #about .image, #about .text,
-    #services header, #services .card,
+    #about .image, #about ,
+    #services header, #services .cards,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #cotnact .links`,
+    #contact .text, #contact .links,
+    footer .logo, footer .brand, footer .social`,
      {interval: 100})
+
+//Back to top
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+    if(this.window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+// Shadown header scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    if(this.window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+}
+
+
+// Active link
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
+})
+
+
